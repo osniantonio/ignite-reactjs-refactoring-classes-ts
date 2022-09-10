00b4c335c73aaa49145603070715d1647cfd0c39
osni.antonio <osni.antonio@gmail.com>
2022-09-10 16:42:24 -0300

    Resolvendo o desafio

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-"# ignite-reactjs-refactoring-classes-ts" 
+# Desafio 02 - Refactoring de classes e typescript
+Nesse desafio, você deverá criar uma aplicação para treinar o que aprendeu até agora no ReactJS
+
+Essa será uma aplicação já funcional onde o seu principal objetivo é realizar dois processos de migração: de Javascript para Typescript e de Class Components para Function Components.
+
+# Como usar
+
+```bash
+# Clone o Repositório
+$ git clone https://github.com/osniantonio/ignite-reactjs-refactoring-classes-ts.git
+```
+
+```bash
+# Baixe as dependendências
+$ yarn
+```
+
+```bash
+# Instale o JSON Server seguindo esses passos:
+
+https://github.com/typicode/json-server#getting-started
+```
+
+```bash
+# Com o JSON Server instalado, inicie o servidor utilizando ele na porta 3333:
+
+$ json-server server.json -p 3333
+```
+
+```bash
+# Execute o projeto em outro terminal
+$ yarn start
+```
+
+Acesse <http://localhost:3000> para ver o resultado.
+
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState, useCallback } from "react";
+
+import { useField } from "@unform/core";
+
+import { IInputProps } from "./@interfaces";
+
+import { Container } from "./styles";
+
+export default function Input({ name, icon: Icon, ...rest }: IInputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+}
